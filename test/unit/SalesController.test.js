13b73aee3f4493000cb29273689f8c2b3eac8e89
achtenberg16 <achtenberg16@gmail.com>
2022-05-26 15:05:36 -0300

    tests: adiconado testes do sales controller

--- test/unit/SalesController.test.js
+++ test/unit/SalesController.test.js
@@ -25,5 +25,53 @@ describe('controllers sales', () => {
      expect(next.calledWith({ message: 'Sale not found', status: 404 })).to.be.true;
     })
 
+    it('se o model retorna uma venda quando da tudo certo', async() => {
+      sinon.stub(salesService, 'getById').resolves({saleId: 1, productId: 1, quantity: 1});
+      await salesController.getById(req, res, next);
+      expect(res.status.calledWith(200)).to.be.true;
+      expect(res.json.calledWith({saleId: 1, productId: 1, quantity: 1})).to.be.true;
+    })
+  })
+
+  describe('getAll', () => {
+    const res = {};
+    const req = {};
+    beforeEach(async () => {
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub()
+    })
+
+    it('testa se um array é retornado', async() => {
+     sinon.stub(salesService, 'getAll').resolves([{saleId: 1, productId: 1, quantity: 1}]);
+     await salesController.getAll(req, res);
+     expect(res.status.calledWith(200)).to.be.true;
+     salesService.getAll.restore();
+    })
+  })
+
+  describe('insertSale', () => {
+    const res = {};
+    const req = {body: [{productId: 1, quantity: 1}]};
+    before(async () => {
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub()
+      next = sinon.stub()
+    }) 
+    
+    it("quando a quantidade não esta disponivel", async() => {
+      sinon.stub(salesService, 'insertSales').resolves({error: 'Product not available'});
+      await salesController.insertSales(req, res, next);
+      expect(next.calledWith({ message: 'Product not available', status: 422 })).to.be.true;
+      salesService.insertSales.restore()
+    })
+
+    it('quando a quantidade esta disponivel', async() => {
+      sinon.stub(salesService, 'insertSales').resolves(1);
+      await salesController.insertSales(req, res, next);
+      expect(res.status.calledWith(201)).to.be.true;
+      expect(res.json.calledWith({id: 1, itemsSold: req.body})).to.be.true;
+      salesService.insertSales.restore()
+    })
+  
   })
 })
